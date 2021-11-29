1bf58567cca4bd35a2e24f422047b84161703419
masacoco <masaonakama0329@gmail.com>
2021-11-29 17:28:43 +0900

    [Finish]lesson4

--- GetFamiliarwithSWIFTUI/ContentView.swift
+++ GetFamiliarwithSWIFTUI/ContentView.swift
@@ -10,16 +10,30 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        Text(/*@START_MENU_TOKEN@*/"Hello!"/*@END_MENU_TOKEN@*/)
-            .padding(.all)
-            .background(Color.green
-                            .blur(radius: /*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/))
-
+        VStack() {
+            
+            Spacer()
+                    
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Spacer()
+                    
+            HStack() {
+                Spacer()
+                Text("Hello!")
+                Spacer()
+                Text("World!")
+                Spacer()
+                   }
+                }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.portrait)
     }
 }
