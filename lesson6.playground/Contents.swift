44a83d200ea3517a69085c6f361dbef65909b8f8
masacoco <masaonakama0329@gmail.com>
2021-12-04 01:01:36 +0900

    [add]lesson9

--- lesson6.playground/Contents.swift
+++ lesson6.playground/Contents.swift
@@ -32,23 +32,70 @@ func myFunc(a:Int, b:Int) -> Int {
 //lesson8
 
 
-struct ChatView{
-//    Properties
+//struct ChatView{
+////    Properties
+//    var message = ""
+//    var messageWithPrefix:String{
+//        let prefix = "Chris says:"
+//        return prefix + message
+//    }
+//
+////    View Code for this screen
+//
+////    Method
+//    func sendChat(){
+////        Code to send the chat message
+//        print(messageWithPrefix)
+//    }
+//
+//    func deleteChat(){
+//        print(messageWithPrefix)
+//    }
+//}
+
+//lesson9
+struct MyStructure{
+
     var message = ""
-    var messageWithPrefix:String{
-        let prefix = "Chris says:"
-        return prefix + message
+    
+    func myFunction(){
+    
+    print(message)
+ }
+}
+
+var a:MyStructure = MyStructure()
+a.message = "Hi"
+a.myFunction()
+
+var b = MyStructure()
+b.message = "world"
+print(b.message)
+
+
+struct DatabaseManager{
+    
+    private var serverName = "Server 1"
+    
+    func saveData(data:String) -> Bool{
+        
+        print(serverName)
+        
+//This code saves the data and returns a boolean result
+        return true
     }
     
-//    View Code for this screen
+}
+
+
+struct ChatView{
+    var message = "Hello"
     
-//    Method
     func sendChat(){
-//        Code to send the chat message
-        print(messageWithPrefix)
-    }
-    
-    func deleteChat(){
-        print(messageWithPrefix)
+        
+        var db = DatabaseManager()
+        let successful = db.saveData(data: message)
+        
+//Check the successful boolean value, if unsuccessful, show alert to user
     }
 }
